bbd78aa4f6dd9eea6032bbbd696227bb1b13b2be
Sergey <terehovs@gmail.com>
2021-04-04 23:19:46 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import random
+
+from flask import Flask, render_template, abort
+
+from data import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    list = tours.keys()
+    otbor = random.sample(list, 6)
+    return render_template('index.html', tours=tours, otbor=otbor, departures=departures)
+
+
+@app.route('/departures/<departure>/')
+def departure(departure):
+    departure_name = departures.get(departure)
+    if departure_name is None:
+        abort(404, "Заданное направление не найдено")
+    return render_template('departure.html', departure=departure, departure_name=departure_name, tours=tours,
+                           departures=departures)
+
+
+@app.route('/tours/<int:tour_id>/')
+def tour(tour_id):
+    tour = tours.get(tour_id)
+    if tour is None:
+        abort(404, "Тур не найден")
+    return render_template('tour.html', tour=tour,
+                           departure_name=departures.get(tours.get(tour_id).get('departure')), departures=departures)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
